f8863363583fdececca893a1b6673f4b9efb32a2
TLPLTSzft <tlpltszft@gmail.com>
2022-10-08 01:27:32 +0200

    Változók felvétele

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private Button buttonPlus;
+  private Button buttonMinus;
+  private TextView textViewCounter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    init();
+
+
+  }
+
+  public void init() {
+    buttonPlus = findViewById(R.id.buttonPlus);
+    buttonMinus = findViewById(R.id.buttonMinus);
+    textViewCounter = findViewById(R.id.textViewCounter);
   }
 }
