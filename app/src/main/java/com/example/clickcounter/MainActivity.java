dec72d47bbf5bfc1fcbbac8833ba25e3ae786379
TLPLTSzft <tlpltszft@gmail.com>
2022-10-08 02:06:24 +0200

    Buttons és Counter működik

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,13 +12,38 @@ public class MainActivity extends AppCompatActivity {
   private Button buttonPlus;
   private Button buttonMinus;
   private TextView textViewCounter;
+  public int szam = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
     init();
 
+    buttonPlus.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        szam++;
+        textViewCounter.setText(String.valueOf(szam));
+      }
+    });
+
+    buttonMinus.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        szam--;
+        textViewCounter.setText(String.valueOf(szam));
+      }
+    });
+
+    textViewCounter.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        szam = 0;
+        textViewCounter.setText(String.valueOf(szam));
+      }
+    });
 
   }
 
